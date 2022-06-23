3dff287362efa47d1f1e4e85e9b81298dcb5ade0
hayate-sato <hayate.sato@rakus-partners.co.jp>
2022-06-23 15:05:48 +0900

    first commit

---
+++ src/main/java/com/example/demo/RegistUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistUserApplication.class, args);
+	}
+
+}
